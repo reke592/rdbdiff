89e9b0bd305263b942fabc029cfca9fd3b93f1e0
Erric Rapsing <erric.rapsing@gmail.com>
2025-01-05 18:50:25 +0800

    added seed for test scenarios, use single database service

---
+++ test/seeds/scenarios.sql
@@ -0,0 +1,105 @@
+drop database if exists A;
+drop database if exists B;
+create database A;
+create database B;
+-- no issue
+CREATE TABLE IF NOT EXISTS A.`scenario_no_issue` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT
+);
+CREATE TABLE IF NOT EXISTS B.`scenario_no_issue` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT
+);
+-- missing table
+CREATE TABLE IF NOT EXISTS A.`scenario_missing_table` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT
+);
+-- missing column
+CREATE TABLE IF NOT EXISTS A.`scenario_missing_column` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column_exist` INT DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS B.`scenario_missing_column` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT
+);
+-- mismatch column
+CREATE TABLE IF NOT EXISTS A.`scenario_mismatch_column_type` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column_exist` INT DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS B.`scenario_mismatch_column_type` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column_exist` INT DEFAULT 0
+);
+-- missing index
+CREATE TABLE IF NOT EXISTS A.`scenario_missing_index` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column1` INT DEFAULT 1,
+  `column2` INT DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS B.`scenario_missing_index` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column1` INT DEFAULT 1,
+  `column2` INT DEFAULT 1
+);
+CREATE INDEX ix ON A.scenario_missing_index(`column1`);
+-- mismatch index column
+CREATE TABLE IF NOT EXISTS A.`scenario_same_ix_name_diff_column` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column1` INT DEFAULT 1,
+  `column2` INT DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS B.`scenario_same_ix_name_diff_column` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column1` INT DEFAULT 1,
+  `column2` INT DEFAULT 1
+);
+CREATE INDEX ix ON A.scenario_same_ix_name_diff_column(`column1`);
+CREATE INDEX ix ON B.scenario_same_ix_name_diff_column(`column2`);
+-- mismatch index sequence
+CREATE TABLE IF NOT EXISTS A.`scenario_mismatch_index_sequence` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column1` INT DEFAULT 1,
+  `column2` INT DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS B.`scenario_mismatch_index_sequence` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `column1` INT DEFAULT 1,
+  `column2` INT DEFAULT 1
+);
+CREATE UNIQUE INDEX ux ON A.scenario_mismatch_index_sequence(`column1`, `column2`);
+CREATE UNIQUE INDEX ux ON B.scenario_mismatch_index_sequence(`column2`, `column1`);
+-- missing stored procedure
+DELIMITER |
+CREATE PROCEDURE A.sp_missing(p_0 INT)
+BEGIN
+	SELECT p_0 AS `result`;
+END;
+|
+DELIMITER ;
+-- mismatch stored procedure parameters
+DELIMITER |
+CREATE PROCEDURE A.sp_mismatch_params(p_0 INT)
+BEGIN
+	SELECT p_0 AS `result`;
+END;
+|
+CREATE PROCEDURE B.sp_mismatch_params(p_0 VARCHAR(50))
+BEGIN
+	SELECT p_0 AS `result`;
+END;
+|
+DELIMITER ;
+-- stored procedure whitespaces
+DELIMITER |
+CREATE PROCEDURE A.sp_whitespaces(p_0 INT)
+BEGIN
+	SELECT p_0 AS `result`;
+END;
+|
+CREATE PROCEDURE B.sp_whitespaces(p_0 INT)
+BEGIN
+	  SELECT p_0 
+    AS `result`;
+END;
+|
+DELIMITER ;
